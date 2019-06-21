2623080f553c08710e0e07553878a404d98f9753
celinechole <github@celinechole.com>
2019-06-21 10:51:25 -0400

    add listening port and get request

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('It is a beautiful day');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('It is a beautiful day')
+})
+
+app.listen(3000, function() {
+    console.log('Server started on port 3000')
+})
