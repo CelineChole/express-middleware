662a5303c7dedf08ffd52854d353d583d5d5d3a4
celinechole <github@celinechole.com>
2019-06-21 11:21:30 -0400

    create a middleware logger

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var path = require('path');
 
 var app = express();
 
+var logger = function(req, res, next){
+    console.log('logging...');
+    next();
+}
+
+app.use(logger);
+// Body Parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
     res.send('It is a beautiful day')
 })
