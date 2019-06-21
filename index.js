2f14b41f8ae14afc13ca31ecd19254f07e3f3598
celinechole <github@celinechole.com>
2019-06-21 11:30:24 -0400

    refactor port listening

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ app.get('/', (req, res) => {
     res.send('It is a beautiful day')
 });
 
+const PORT = process.env.PORT || 5000;
 
 
-app.listen(3000, function() {
-    console.log('Server started on port 3000')
-})
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
