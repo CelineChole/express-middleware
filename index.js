7a5d106e93e1654cbfb58eb824fbd9bcd4fcb449
celinechole <github@celinechole.com>
2019-06-21 12:24:33 -0400

    add a logger

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const path = require('path');
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log('I am a logger');
+    next();
+}
+
+// init middleware
+app.use(logger);
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000;
