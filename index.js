498d5fe8f9ff037745a35f358643eba56c463f3e
celinechole <github@celinechole.com>
2019-06-21 11:28:50 -0400

    use arrow functions

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+const express = require('express');
 
-var app = express();
+const app = express();
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.send('It is a beautiful day')
-})
+});
+
+
 
 app.listen(3000, function() {
     console.log('Server started on port 3000')
